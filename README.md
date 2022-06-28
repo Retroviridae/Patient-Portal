0b0c0f1db32fc13c4b3687e6247a412904ec06d4
Alexander Schuessler <schuessler.alex@gmail.com>
2022-06-27 19:53:54 -0500

    Trying to fix heroku issues.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ https://dbdiagram.io/d
 https://github.com/learn-co-curriculum/react-rails-project-setup-guide
 
 sudo service postgresql start
+
+https://phase-4-project-patient-portal.herokuapp.com/
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import MyPrescriptions from './MyPrescriptions';
+import { Form, Button} from 'react-bootstrap/';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Home(){
 
     return (
         <header>
+             {/* <Row className='mt-2'>
+        <Form.Label column lg={2}>
+          Date
+        </Form.Label>
+        <Col xs={3}>
+          <Form.Control type="time" placeholder="Date" name='date' onChange={(e)=>console.log(e.target.value)} />
+        </Col>
+      </Row>
+
+
+      <Row className='mt-2'>
+        <Form.Label column lg={2}>
+          Category
+        </Form.Label>
+        <Col xs={7}>
+          <Form.Select name='category'  aria-label="Default select example" >
+            <option >Category</option>
+            <option value="Cardio">Cardio</option>
+            <option value="Weights">Weights</option>
+            <option value="Sports">Sports</option>
+            <option value="Body Weight">Body Weight</option>
+          </Form.Select>
+        </Col>
+      </Row> */}
         </header>
     )
 }
